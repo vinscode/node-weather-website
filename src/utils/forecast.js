d9df72ee2c5a489e2c819822bf0f759edd8b2538
Vinaydv <vinaydoggali@gmail.com>
2021-06-17 23:10:15 +1000

    Adding additional data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=acacfd7fee15812b14952f92aea9d2c9&query=' + encodeURIComponent(longitude) +',' + encodeURIComponent(latitude) +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=acacfd7fee15812b14952f92aea9d2c9&query=' + encodeURIComponent(longitude) +',' + encodeURIComponent(latitude) +'&units=m'
 
     request({ url, json: true}, (error, { body}) => {
         if(error){    
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location')
         }else {
-            callback(undefined, body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees and feels like '+body.current.feelslike+'.'
+            callback(undefined, body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+'C and feels like '+body.current.feelslike+'C with '+body.current.humidity+'% humidity and UV index = '+body.current.uv_index+ '.'
             )
         }
     })
